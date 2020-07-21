d8fe137d0a375184bc26529a782d4816cbcc6fc6
Sum424 <sumanthapa326@gmail.com>
2020-07-21 10:41:50 +0545

    First two lessons

---
+++ app.js
@@ -0,0 +1,13 @@
+/* Vue Instance
+    el: "" --for element to control
+    data: --for storing object
+*/
+new Vue({
+    el: '.vue-app',
+    data: {
+        name: 'Suman'
+    }
+
+});
+
+/* Data and methods */
